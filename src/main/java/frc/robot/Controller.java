32d16f0641a87894c647dc9335e8411ce0758fc2
BlueZeeKing <github@blueish.dev>
2023-03-02 15:45:53 -0500

    Dead zones

--- src/main/java/frc/robot/Controller.java
+++ src/main/java/frc/robot/Controller.java
@@ -5,12 +5,14 @@ import edu.wpi.first.wpilibj.XboxController;
 public class Controller {
     private final XboxController controller = new XboxController(0);
 
+    private static final double DEAD_ZONE_SIZE = 0.05;
+
     public double getForward() {
-        return controller.getLeftY();
+        return deadZones(DEAD_ZONE_SIZE, controller.getLeftY());
     }
 
     public double getTurn() {
-        return controller.getRightX();
+        return deadZones(DEAD_ZONE_SIZE, controller.getRightX());
     }
 
     public boolean getArmUp() {
@@ -27,5 +29,13 @@ public class Controller {
 
     public boolean getIntakeBackward() { // TODO: figure out trigger buttons
         return controller.getBButtonPressed();
+    }
+
+    private double deadZones(double size, double value) {
+        if (Math.abs(value) < size) {
+            return 0;
+        }
+
+        return value;
     }
 }
