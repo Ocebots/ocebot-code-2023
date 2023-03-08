6d3b0649522fd30e36e0e3510f8978f339f0ffec
BlueZeeKing <github@blueish.dev>
2023-03-08 16:57:15 -0500

    Add skeleton gyro code

---
+++ src/main/java/frc/robot/hardware/Gyroscope.java
@@ -0,0 +1,15 @@
+package frc.robot.hardware;
+
+public class Gyroscope {
+  public Gyroscope() {
+
+  }
+
+  public boolean verify() {
+    throw new Error("Not done yet");
+  }
+
+  public double getPitch() {
+    throw new Error("Not done yet");
+  }
+}
