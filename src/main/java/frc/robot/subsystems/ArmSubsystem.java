6ec7b1069c23c2c0842e7010004b663e820aac20
BlueZeeKing <github@blueish.dev>
2023-03-09 10:56:54 -0500

    Implement autocloseable on subsystems

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.commands.abstracts.TimedCommandBuilder;
 
-public class ArmSubsystem extends SubsystemBase {
+public class ArmSubsystem extends SubsystemBase implements AutoCloseable {
     private final CANSparkMax arm = new CANSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushless);
 
     private static final int AMP_LIMIT = 20;
@@ -30,5 +30,11 @@ public class ArmSubsystem extends SubsystemBase {
 
     public CommandBase lowerArm() {
         return armCommand(false);
+    }
+
+    @Override
+    public void close() throws Exception {
+        arm.set(0);
+        arm.close();
     }
 }
