13f574f68f7ef9be1064427cac8c8c9a747c5cd1
BlueZeeKing <github@blueish.dev>
2023-03-17 15:37:37 -0400

    Add timeouts

--- src/main/java/frc/robot/commands/AutoBalanceCommand.java
+++ src/main/java/frc/robot/commands/AutoBalanceCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.abstracts.TimedCommandBuilder;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.GyroSubsystem;
 
@@ -9,9 +10,9 @@ public class AutoBalanceCommand {
     public static Command getCommand(DrivetrainSubsystem drivetrainSubsystem, GyroSubsystem gyro) {
         return new SequentialCommandGroup(
                 gyro.setLevelCommand(),
-                drivetrainSubsystem.driveBackward(1).until(() -> !gyro.isLevel()),
+                TimedCommandBuilder.of(drivetrainSubsystem.driveBackward(1).until(() -> !gyro.isLevel()), 1.5),
                 drivetrainSubsystem.driveMotorIdleCommand(true),
-                drivetrainSubsystem.driveBackward(0.3).until(gyro::isLevel)
+                TimedCommandBuilder.of(drivetrainSubsystem.driveBackward(0.3).until(gyro::isLevel), 4)
         );
     }
 }
